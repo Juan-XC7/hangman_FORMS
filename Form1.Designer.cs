9c0cf7277e754135e918dccf0e41eca2d9fcdf32
Juan-XC7 <113059155+Juan-XC7@users.noreply.github.com>
2024-03-12 23:24:41 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace hungman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.answerButton = new System.Windows.Forms.Button();
+            this.answerBox = new System.Windows.Forms.TextBox();
+            this.questionBox = new System.Windows.Forms.TextBox();
+            this.hangmanPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(264, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(345, 248);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 75);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(426, 248);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 75);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(264, 167);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 75);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(345, 167);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 75);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(426, 167);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 75);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(264, 86);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 75);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(345, 86);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 75);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(426, 86);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 75);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button0
+            // 
+            this.button0.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button0.Location = new System.Drawing.Point(264, 329);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(156, 75);
+            this.button0.TabIndex = 9;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // answerButton
+            // 
+            this.answerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerButton.Location = new System.Drawing.Point(426, 329);
+            this.answerButton.Name = "answerButton";
+            this.answerButton.Size = new System.Drawing.Size(75, 75);
+            this.answerButton.TabIndex = 10;
+            this.answerButton.Text = "↩";
+            this.answerButton.UseVisualStyleBackColor = true;
+            this.answerButton.Click += new System.EventHandler(this.answerButton_Click);
+            // 
+            // answerBox
+            // 
+            this.answerBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerBox.Location = new System.Drawing.Point(264, 27);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.Size = new System.Drawing.Size(237, 53);
+            this.answerBox.TabIndex = 14;
+            // 
+            // questionBox
+            // 
+            this.questionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionBox.Location = new System.Drawing.Point(21, 27);
+            this.questionBox.Name = "questionBox";
+            this.questionBox.Size = new System.Drawing.Size(237, 53);
+            this.questionBox.TabIndex = 15;
+            // 
+            // hangmanPicture
+            // 
+            this.hangmanPicture.Location = new System.Drawing.Point(21, 86);
+            this.hangmanPicture.Name = "hangmanPicture";
+            this.hangmanPicture.Size = new System.Drawing.Size(237, 318);
+            this.hangmanPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.hangmanPicture.TabIndex = 16;
+            this.hangmanPicture.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1195, 569);
+            this.Controls.Add(this.hangmanPicture);
+            this.Controls.Add(this.questionBox);
+            this.Controls.Add(this.answerBox);
+            this.Controls.Add(this.answerButton);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button answerButton;
+        private System.Windows.Forms.TextBox answerBox;
+        private System.Windows.Forms.TextBox questionBox;
+        private System.Windows.Forms.PictureBox hangmanPicture;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace hungman
+{
+    public partial class Form1 : Form
+    {
+        public int errorCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            questionBox.Text = GenerateRandomOperation();
+            updateHangmanImage();
+        }
+
+        private static readonly Random random = new Random(); // Single instance
+
+        public static int GenerateRandomNumber(int minValue, int maxValue)
+        {
+            if (minValue > maxValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(minValue), "Minimum value cannot be greater than maximum value.");
+            }
+
+            return random.Next(minValue, maxValue + 1);
+        }
+
+        public static string GenerateRandomOperation()
+        {
+            int value1 = GenerateRandomNumber(1, 10);
+            int value2 = GenerateRandomNumber(1, 10);
+
+            string operation = value1.ToString() + "x" + value2.ToString();
+            return operation;
+        }
+
+        private void SetInputItem(string item)
+        {
+            string currentValueText = answerBox.Text;
+            string newValue = item.ToString();
+
+            currentValueText += newValue;
+            answerBox.Text = currentValueText;
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            SetInputItem("0");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetInputItem("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetInputItem("2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SetInputItem("3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SetInputItem("4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SetInputItem("5");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SetInputItem("6");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SetInputItem("7");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            SetInputItem("8");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            SetInputItem("9");
+        }
+
+        public static int getOperationResult(object sender, EventArgs e, TextBox textBox)
+        {
+            string input = textBox.Text;
+            int operatorIndex = input.IndexOf('x'); // Find the index of the multiplication operator
+
+            if (operatorIndex == -1)
+            {
+                throw new ArgumentException("Multiplication operator (x) not found in input.");
+            }
+
+            // Get first number
+            string firstNumberString = input.Substring(0, operatorIndex);
+            int number1 = int.Parse(firstNumberString);
+
+            // Get second number
+            string secondNumberString = input.Substring(operatorIndex + 1);
+            int number2 = int.Parse(secondNumberString);
+
+            return number1 * number2;
+        }
+
+        private void cleanAnswerBox()
+        {
+            answerBox.Text = "";
+        }
+
+        private void answerButton_Click(object sender, EventArgs e)
+        {
+            int userAnswer = int.Parse(answerBox.Text);
+            int operationResult = getOperationResult(this, EventArgs.Empty, questionBox);
+
+            if (userAnswer == operationResult)
+            {
+                MessageBox.Show($"Congrats! you've got the correct answer!", "Correct answer!");
+            }
+            else
+            {
+                errorCounter++;
+                updateHangmanImage();
+                MessageBox.Show($"The answer was: {operationResult}, your answer: {userAnswer}", "Wrong answer!");
+            }
+
+            questionBox.Text = GenerateRandomOperation();
+            cleanAnswerBox();
+        }
+        private void updateHangmanImage()
+        {
+            switch (errorCounter)
+            {
+                case 0: hangmanPicture.Image = Properties.Resources._0_err; break;
+                case 1: hangmanPicture.Image = Properties.Resources._1_err; break;
+                case 2: hangmanPicture.Image = Properties.Resources._2_err; break;
+                case 3: hangmanPicture.Image = Properties.Resources._3_err; break;
+                case 4: hangmanPicture.Image = Properties.Resources._4_err; break;
+                case 5: hangmanPicture.Image = Properties.Resources._5_err; break;
+                case 6: hangmanPicture.Image = Properties.Resources._6_err; break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
